8f5bbf9d5f4d0285cd3656fa8002bc1e789823ae
mariounisa77 <32866091+mariounisa77@users.noreply.github.com>
2018-07-11 14:02:32 +0200

    Update bluetooth

--- bluetooth.js
+++ bluetooth.js
@@ -1,7 +1,26 @@
 	//BLE
-	let characteristicCache = null;
+	
+	let staticButton = document.getElementById('staticButton');
+    let characteristicCache = null;
     let deviceCache = null;
+    var last_command = "000#000#";
+	
+	var timer = new Timer(function() {
+		if (last_command != command){
+			writeToCharacteristic(characteristicCache, command);
+			console.log(command);
+			last_command = command;
+		}	
+	}, 100);
 
+    window.onload = () => {
+		if(!navigator.bluetooth){
+			alert('Your current browser does not support web bluetooth or is not enabled. Please use the latest version of Chrome and enable Web Bluetooth under chrome://flags');
+			connectBtn.disabled = true;
+		}
+		connectBtn.onclick = connect;
+	}
+    
 	function connect() {
 			return (deviceCache ? Promise.resolve(deviceCache) :
 		  requestBluetoothDevice()).
@@ -47,4 +66,19 @@
 	    		handleCharacteristicValueChanged);
 			});
 	}
+ 
+	var timesClicked = 2;
 
+    function staticFunction(){
+		if (timesClicked%2==0) {
+			timesClicked++;
+			command = "100#00#";
+			setTimeout(function(){
+				timer.stop();
+			}, 250);
+		}else{
+			timesClicked++;
+			command = "101#00#";
+			timer.start();
+		}
+	}
