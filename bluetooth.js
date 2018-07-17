be797f2a2d5708a4292fa1d3755717ab4555c52d
mariounisa77 <32866091+mariounisa77@users.noreply.github.com>
2018-07-17 10:03:47 +0200

    Update BLE

--- bluetooth.js
+++ bluetooth.js
@@ -1,4 +1,9 @@
-	var timer = new Timer(function() {
+//Variabili js	
+let characteristicCache = null;
+let deviceCache = null;
+var command = "000";
+
+var timer = new Timer(function() {
 		if (command != "000"){
 			writeToCharacteristic(characteristicCache, command);
 			console.log(command);
@@ -8,30 +13,27 @@
         }	
 	}, 300);
 
-    window.onload = () => {
+window.onload = () => {
         
         //BLE
 	   let connectBtn = document.getElementById('connect');
 	   let staticButton = document.getElementById('staticButton');
-        let characteristicCache = null;
-        let deviceCache = null;
-        var command = "000";
         
 		if(!navigator.bluetooth){
 			alert('Your current browser does not support web bluetooth or is not enabled. Please use the latest version of Chrome and enable Web Bluetooth under chrome://flags');
 			connectBtn.disabled = true;
 		}
 		connectBtn.onclick = connect;
-	}
+}
     
-	function connect() {
+function connect() {
 			return (deviceCache ? Promise.resolve(deviceCache) :
 		  requestBluetoothDevice()).
 		  then(device => connectDeviceAndCacheCharacteristic(device)).
 		  then(characteristic => startNotifications(characteristic)).
 		  catch(error => console.log(error));
-	}
-    function start_dynamic_evaluation(){
+}
+function start_dynamic_evaluation(){
 		var dyn_btn = document.getElementById("dynamicButton");
         if(document.getElementById("dynamicButton").innerHTML == "Stop"){
             command = "201";
@@ -47,16 +49,14 @@
             disableButtons()
             dyn_btn.disabled = false;
         }
-        
-
-    }
+}
    
-    function calibra_acc(){
+function calibra_acc(){
         document.getElementById("calibrazione-text").innerHTML = "Calibrazione effettuata!";
 		command = "300";
-        
-    }
-	function requestBluetoothDevice() {
+}
+
+function requestBluetoothDevice() {
 		  return navigator.bluetooth.requestDevice({
 		    filters: [{services: [0xFFE0]}],
 		  }).
@@ -64,12 +64,12 @@
 		        deviceCache = device;
 		        return deviceCache;
 		    	});
-	}
+}
 
-	function connectDeviceAndCacheCharacteristic(device) {
+function connectDeviceAndCacheCharacteristic(device) {
 		  if (device.gatt.connected && characteristicCache) {
 		    return Promise.resolve(characteristicCache);
-		  }
+          }
 
 		  return device.gatt.connect().
 		      then(server => {
@@ -83,9 +83,9 @@
 
 		        return characteristicCache;
 		      });
-	}
+}
 
-	function startNotifications(characteristic) {
+function startNotifications(characteristic) {
             document.getElementById("connessione-text").innerHTML = "Connesso con Smart Insole!";
             enableButtons();
 
@@ -95,11 +95,11 @@
 	    		handleCharacteristicValueChanged);
 			});
 
-	}
+}
  
 
 
-    function staticFunction(){
+function staticFunction(){
         
         disableButtons();
 
@@ -109,12 +109,12 @@
 			/*setTimeout(function(){
 				timer.stop();
 			}, 250);*/
-	}
+}
 
 
-    let readBuffer = '';
+let readBuffer = '';
 
-	function handleCharacteristicValueChanged(event) {
+function handleCharacteristicValueChanged(event) {
 		let value = new TextDecoder().decode(event.target.value);
 	  	for (let c of value) {
 	    	if (c === '\n') {
@@ -129,12 +129,13 @@
 		      readBuffer += c;
 		    }
 		  }
-	}		
-    function receive(data) {
-  		log(data, 'in');
-	}
+}		
 
-	function log(data, type = '') {
+function receive(data) {
+  		log(data, 'in');
+}
+
+function log(data, type = '') {
        //{\"code\" : \"1\", \"weight_perc\" : {\"avan_sx\":\"%d\",\"avan_dx\":\"%d\",\"back\":\"%d\"}\"}		
         var json_data = JSON.parse(data);
         
@@ -166,33 +167,34 @@
             addDynamicData(dynamic_chart,avan_sx,avan_dx,back);
             console.log("Dynamic data added");
         }
-	}
+}
 
-	function writeToCharacteristic(characteristic, str) {
+function writeToCharacteristic(characteristic, str) {
 			characteristic.writeValue(str2ab(str));
 
-	}
+}
 
-	function str2ab(str){
+function str2ab(str){
 		let buf = new ArrayBuffer(str.length); // 2 bytes for each char
 		let bufView = new Uint8Array(buf); //make sure buffer array is of type uint8
 		for (let i=0, strLen=str.length; i < strLen; i++) {
 			bufView[i] = str.charCodeAt(i);
 		}
 		return buf;
-	}
+}
 
-    function disableButtons(){
+function disableButtons(){
         
         document.getElementById("calibrationButton").disabled = true;
         document.getElementById("staticButton").disabled = true;
         document.getElementById("dynamicButton").disabled = true;
         
-    }
-    function enableButtons(){
+}
+
+function enableButtons(){
         
         document.getElementById("calibrationButton").disabled = false;
         document.getElementById("staticButton").disabled = false;
         document.getElementById("dynamicButton").disabled = false;
         
-    }
+}
