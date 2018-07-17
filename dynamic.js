6186a502621052fc5b6701d7fffbe2baf6b3c93a
mariounisa77 <32866091+mariounisa77@users.noreply.github.com>
2018-07-17 12:39:52 +0200

    Update dynamic

--- dynamic.js
+++ dynamic.js
@@ -66,18 +66,38 @@ scales: {
 function addDynamicData(dynamic_chart,av_sx,av_dx,back){
                 
                 var n = n_passo;
+                var corretto = false;
                 n_passo++;
+                document.getElementById("numero_passi").innerHTML = n;
                 
+    
                 var label = n+"°";
 				dynamic_chart.data.labels.push(label);
                 
                 if(back>45){
+                    if(back<49){
+                        corretto = true;   
+                    }
                     dynamic_chart.data.datasets[0].data[n] = -back+45;
                 }else if(back<45){
+                    if(av_sx+av_dx-55<4){
+                        corretto = true;   
+                    }
                    dynamic_chart.data.datasets[0].data[n] = (av_sx+av_dx-55);
                 }else{
                     dynamic_chart.data.datasets[0].data[n] = 0;
                 }
-
+                if(corretto){
+                   var n = document.getElementById("numero_passi_corretti").innerHTML() + 1;
+                   document.getElementById("numero_passi_corretti").innerHTML("n");
+                }else{
+                    var n = document.getElementById("numero_passi_sbagliati").innerHTML() + 1;
+                   document.getElementById("numero_passi_sbagliati").innerHTML("n");
+                }
 				dynamic_chart.update();
+    
+    
+    
+    
+    
 };
